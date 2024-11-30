a42651487d4ec50e0068bc1df6be0f201eb30861
Louis Amoah-Nuamah <73677428+louisan42@users.noreply.github.com>
2024-11-30 18:32:20 -0500

    add imports from spring security

--- backend/yearbook/hooks/pre-commit.sh
+++ backend/yearbook/hooks/pre-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo '[Git Hook] Executing Spotless check before commit'
 # Only compare files that have changed compared to main
-mvnw spotless:check -DratchetFrom=origin/main
+mvnw spotless:check #-DratchetFrom=origin/main
 exit $?
