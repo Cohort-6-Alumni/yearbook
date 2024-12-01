fa991e050b9f9e6bf4c8a77630784361acb148a4
Louis Amoah-Nuamah <73677428+louisan42@users.noreply.github.com>
2024-11-30 23:59:16 -0500

    update commit hook

--- backend/yearbook/hooks/pre-commit.sh
+++ backend/yearbook/hooks/pre-commit.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 echo '[Git Hook] Executing Spotless check before commit'
 # Only compare files that have changed compared to main
-mvnw spotless:check #-DratchetFrom=origin/main
+#mvnw spotless:check
+backend/yearbook/mvnw spotless:check -DratchetFrom=origin/main
+if [ $? -ne 0 ]; then
+   echo "Spotless check failed...."
+   echo "Running Spotless Formatter to fix the issues....."
+
+   backend/yearbook/mvnw spotless::apply
+    if [ $? -ne 0 ]; then
+        echo "Spotless Formatter failed...."
+        echo "Please fix the issues and commit again...."
+        exit 1
+    fi
+    echo "Code formatted successfully"
+    echo "Committing changes....."
+
+fi
 exit $?
