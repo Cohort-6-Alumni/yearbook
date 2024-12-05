61d1ae178625051c51b3a70d16275ed4a05e7b2a
Amy-nwoke <awesomeamy300@gmail.com>
2024-12-05 12:13:12 -0500

    Commit

---
+++ backend/src/main/java/com/obsidi/yearbook/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.obsidi.yearbook.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.springframework.stereotype.Service;
+import com.obsidi.yearbook.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.obsidi.yearbook.jpa.User;
+import java.util.Optional;
+
+@Service("UserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		Optional<User> opt = this.userRepository.findByUsername(username);
+
+		if (opt.isEmpty()) {
+			throw new UsernameNotFoundException("Username not found: " + username);
+		}
+
+		return new CustomUserDetails(opt.get());
+	}
+
+}
