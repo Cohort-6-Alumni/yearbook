9d7c3827d78e35a4057658b7274517f002018be9
Amy-nwoke <awesomeamy300@gmail.com>
2024-12-06 14:12:25 -0500

    code format

--- backend/src/main/java/com/obsidi/yearbook/config/WebSecurityConfig.java
+++ backend/src/main/java/com/obsidi/yearbook/config/WebSecurityConfig.java
@@ -1,93 +1,105 @@
 package com.obsidi.yearbook.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 
-
-import org.springframework.context.annotation.Configuration;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.obsidi.yearbook.filter.CustomAuthEntryPoint;
 import com.obsidi.yearbook.filter.JwtAuthorizationFilter;
 import com.obsidi.yearbook.provider.ResourceProvider;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import java.util.Arrays;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import static org.springframework.security.config.Customizer.withDefaults;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	@Autowired
-	ResourceProvider provider;
+  @Autowired ResourceProvider provider;
 
-	@Autowired
-	JwtAuthorizationFilter jwtAuthorizationFilter;
+  @Autowired JwtAuthorizationFilter jwtAuthorizationFilter;
 
-	@Autowired
-	CustomAuthEntryPoint customAuthEntryPoint;
+  @Autowired CustomAuthEntryPoint customAuthEntryPoint;
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-	@Bean
-	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-			throws Exception {
-		return authenticationConfiguration.getAuthenticationManager();
-	}
+  @Bean
+  AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 
-	@Bean
-	MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
-		return new MvcRequestMatcher.Builder(introspector);
-	}
+  @Bean
+  MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+    return new MvcRequestMatcher.Builder(introspector);
+  }
 
-	private RequestMatcher[] getMatchers(MvcRequestMatcher.Builder mvc) {
+  private RequestMatcher[] getMatchers(MvcRequestMatcher.Builder mvc) {
 
-		return Arrays.stream(this.provider.getJwtExcludedUrls()).map(url -> mvc.pattern(url))
-				.toArray(RequestMatcher[]::new);
-	}
+    return Arrays.stream(this.provider.getJwtExcludedUrls())
+        .map(url -> mvc.pattern(url))
+        .toArray(RequestMatcher[]::new);
+  }
 
-	@Bean("test")
-	SecurityFilterChain securityFilterChainTest(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+  @Bean("test")
+  SecurityFilterChain securityFilterChainTest(HttpSecurity http, MvcRequestMatcher.Builder mvc)
+      throws Exception {
 
-		http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests((requests) -> requests.requestMatchers(PathRequest.toH2Console()).permitAll()
-						.requestMatchers(this.getMatchers(mvc)).permitAll().anyRequest().authenticated())
-				.exceptionHandling((handler) -> handler.authenticationEntryPoint(this.customAuthEntryPoint))
-				.addFilterBefore(this.jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
-				.headers((headers) -> headers.frameOptions((frame) -> frame.sameOrigin())).cors(withDefaults())
-				.csrf((csrf) -> csrf.disable());
+    http.sessionManagement(
+            (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            (requests) ->
+                requests
+                    .requestMatchers(PathRequest.toH2Console())
+                    .permitAll()
+                    .requestMatchers(this.getMatchers(mvc))
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .exceptionHandling((handler) -> handler.authenticationEntryPoint(this.customAuthEntryPoint))
+        .addFilterBefore(this.jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+        .headers((headers) -> headers.frameOptions((frame) -> frame.sameOrigin()))
+        .cors(withDefaults())
+        .csrf((csrf) -> csrf.disable());
 
-		return http.build();
-	}
+    return http.build();
+  }
 
-	@Bean
-	@ConditionalOnMissingBean(SecurityFilterChain.class)
-	SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+  @Bean
+  @ConditionalOnMissingBean(SecurityFilterChain.class)
+  SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc)
+      throws Exception {
 
-		http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests((requests) -> requests.requestMatchers(this.getMatchers(mvc)).permitAll()
-						.anyRequest().authenticated())
-				.exceptionHandling((handler) -> handler.authenticationEntryPoint(this.customAuthEntryPoint))
-				.addFilterBefore(this.jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
-				.cors(withDefaults()).csrf((csrf) -> csrf.disable());
+    http.sessionManagement(
+            (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            (requests) ->
+                requests
+                    .requestMatchers(this.getMatchers(mvc))
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .exceptionHandling((handler) -> handler.authenticationEntryPoint(this.customAuthEntryPoint))
+        .addFilterBefore(this.jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+        .cors(withDefaults())
+        .csrf((csrf) -> csrf.disable());
 
-		return http.build();
-	}
+    return http.build();
+  }
 }
-
-
--- backend/src/main/java/com/obsidi/yearbook/service/UserService.java
+++ backend/src/main/java/com/obsidi/yearbook/service/UserService.java
@@ -1,15 +1,32 @@
 package com.obsidi.yearbook.service;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+import com.obsidi.yearbook.exception.domain.EmailExistException;
+import com.obsidi.yearbook.exception.domain.UserNotFoundException;
+import com.obsidi.yearbook.exception.domain.UsernameExistException;
+import com.obsidi.yearbook.jpa.Profile;
 import com.obsidi.yearbook.jpa.User;
+import com.obsidi.yearbook.provider.ResourceProvider;
 import com.obsidi.yearbook.repository.UserRepository;
+import com.obsidi.yearbook.security.JwtService;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class UserService {
@@ -17,6 +34,15 @@ public class UserService {
   final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @Autowired private UserRepository userRepository;
+  @Autowired PasswordEncoder passwordEncoder;
+
+  @Autowired EmailService emailService;
+
+  @Autowired AuthenticationManager authenticationManager;
+
+  @Autowired JwtService jwtService;
+
+  @Autowired ResourceProvider provider;
 
   // Fetch all users
   public List<User> listUsers() {
@@ -33,12 +59,34 @@ public class UserService {
     return this.userRepository.save(user);
   }
 
+  private void validateUsernameAndEmail(String username, String emailId) {
+
+    this.userRepository
+        .findByUsername(username)
+        .ifPresent(
+            u -> {
+              throw new UsernameExistException(
+                  String.format("Username already exists, %s", u.getUsername()));
+            });
+
+    this.userRepository
+        .findByEmailId(emailId)
+        .ifPresent(
+            u -> {
+              throw new EmailExistException(
+                  String.format("Email already exists, %s", u.getEmailId()));
+            });
+  }
+
   // Signup method
   public User signup(User user) {
     // Convert username and emailId to lowercase
     user.setUsername(user.getUsername().toLowerCase());
     user.setEmailId(user.getEmailId().toLowerCase());
 
+    this.validateUsernameAndEmail(user.getUsername(), user.getEmailId());
+
+    user.setPassword(this.passwordEncoder.encode(user.getPassword()));
     // Set createdOn to the current timestamp
     user.setCreatedOn(Timestamp.from(Instant.now()));
 
@@ -47,5 +95,160 @@ public class UserService {
 
     // Return the modified user object
     return user;
+  }
+
+  // supplier doesnt take anyvalue but returns a value while consumer consumes and supplies nothing
+  private void updateValue(Supplier<String> getter, Consumer<String> setter) {
+
+    Optional.ofNullable(getter.get())
+        // .filter(StringUtils::hasText)
+        .map(String::trim)
+        .ifPresent(setter);
+  }
+
+  private void updatePassword(Supplier<String> getter, Consumer<String> setter) {
+
+    Optional.ofNullable(getter.get())
+        .filter(StringUtils::hasText)
+        .map(this.passwordEncoder::encode)
+        .ifPresent(setter);
+  }
+
+  private User updateUser(User user, User currentUser) {
+
+    this.updateValue(user::getFirstName, currentUser::setFirstName);
+    this.updateValue(user::getLastName, currentUser::setLastName);
+    this.updateValue(user::getPhone, currentUser::setPhone);
+    this.updateValue(user::getEmailId, currentUser::setEmailId);
+    this.updatePassword(user::getPassword, currentUser::setPassword);
+
+    return this.userRepository.save(currentUser);
+  }
+
+  public User updateUser(User user) {
+
+    String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+    /* Validates the new email if provided */
+    this.userRepository
+        .findByEmailId(user.getEmailId())
+        .filter(u -> !u.getUsername().equals(username))
+        .ifPresent(
+            u -> {
+              throw new EmailExistException(
+                  String.format("Email already exists, %s", u.getEmailId()));
+            });
+
+    /* Get and Update User */
+    return this.userRepository
+        .findByUsername(username)
+        .map(currentUser -> this.updateUser(user, currentUser))
+        .orElseThrow(
+            () -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+  }
+
+  private Authentication authenticate(String username, String password) {
+    return this.authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(username, password));
+  }
+
+  public User authenticate(User user) {
+
+    /* Spring Security Authentication. */
+    this.authenticate(user.getUsername(), user.getPassword());
+
+    /* Get User from the DB. */
+    Optional<User> usr = this.userRepository.findByUsername(user.getUsername());
+    return usr.get();
+    // return this.userRepository.findByUsername(user.getUsername());
+  }
+
+  public HttpHeaders generateJwtHeader(String username) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add(
+        AUTHORIZATION,
+        this.jwtService.generateJwtToken(username, this.provider.getJwtExpiration()));
+
+    return headers;
+  }
+
+  public void resetPassword(String password) {
+
+    String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+    User user =
+        this.userRepository
+            .findByUsername(username)
+            .orElseThrow(
+                () ->
+                    new UserNotFoundException(
+                        String.format("Username doesn't exist, %s", username)));
+
+    user.setPassword(this.passwordEncoder.encode(password));
+
+    this.userRepository.save(user);
+  }
+
+  public User getUser() {
+
+    String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+    /* Get User from the DB. */
+    return this.userRepository
+        .findByUsername(username)
+        .orElseThrow(
+            () -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+  }
+
+  public void sendResetPasswordEmail(String emailId) {
+
+    Optional<User> opt = this.userRepository.findByEmailId(emailId);
+
+    if (opt.isPresent()) {
+      this.emailService.sendResetPasswordEmail(opt.get());
+    } else {
+      logger.debug("Email doesn't exist, {}", emailId);
+    }
+  }
+
+  private User updateUserProfile(Profile profile, User user) {
+
+    Profile currentProfile = user.getProfile();
+
+    if (Optional.ofNullable(currentProfile).isPresent()) {
+
+      this.updateValue(profile::getHeadline, currentProfile::setHeadline);
+      this.updateValue(profile::getBio, currentProfile::setBio);
+      this.updateValue(profile::getInterests, currentProfile::setInterests);
+      this.updateValue(profile::getHobbies, currentProfile::setHobbies);
+      this.updateValue(profile::getPicture, currentProfile::setPicture);
+      this.updateValue(profile::getFavoriteCodingSnack, currentProfile::setFavoriteCodingSnack);
+      this.updateValue(profile::getFavoriteQuote, currentProfile::setFavoriteQuote);
+      this.updateValue(profile::getMostLikelyTo, currentProfile::setMostLikelyTo);
+      this.updateValue(
+          profile::getMostMemorableBootcampMoment, currentProfile::setMostMemorableBootcampMoment);
+      this.updateValue(profile::getAdviceForFutureCohort, currentProfile::setAdviceForFutureCohort);
+      this.updateValue(profile::getBiggestChallenge, currentProfile::setBiggestChallenge);
+      this.updateValue(profile::getHowYouOvercameIt, currentProfile::setHowYouOvercameIt);
+      this.updateValue(profile::getLastWords, currentProfile::setLastWords);
+
+    } else {
+      user.setProfile(profile);
+      profile.setUser(user);
+    }
+
+    return this.userRepository.save(user);
+  }
+
+  public User updateUserProfile(Profile profile) {
+
+    String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+    /* Get and Update User */
+    return this.userRepository
+        .findByUsername(username)
+        .map(user -> this.updateUserProfile(profile, user))
+        .orElseThrow(
+            () -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
   }
 }
